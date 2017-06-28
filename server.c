545b431fc2206e43f2217c4675e50904f1e01951
Mario <CuitMario@users.noreply.github.com>
2017-06-28 20:05:56 +0800

    add fork and interactive with client

--- server.c
+++ server.c
@@ -3,14 +3,19 @@
 #include "arpa/inet.h"
 #include "stdlib.h"
 #include "stdio.h"
+#include "sys/errno.h"
 
+#define PORT 6666
+#define LISTENQ 1024
 #define MAXLINE 4096
 #define SA struct sockaddr
+
+void str_echo(int sockfd);
 
 int main(int argc, char **argv) {
     int     sockfd, connfd;
     socklen_t clilen;
-    char    buff[MAXLINE];
+    pid_t   childpid;
     struct  sockaddr_in servaddr, cliaddr;
 
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
@@ -20,7 +25,7 @@ int main(int argc, char **argv) {
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(6666);
+    servaddr.sin_port = htons(PORT);
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     if(bind(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0) {
@@ -28,7 +33,7 @@ int main(int argc, char **argv) {
         exit(0);
     }
 
-    if(listen(sockfd, 1024) < 0){
+    if(listen(sockfd, LISTENQ) < 0){
         printf("listen sockfd failed!");
         exit(0);
     }
@@ -36,9 +41,28 @@ int main(int argc, char **argv) {
     for( ; ; ) {
         clilen = sizeof(cliaddr);
         connfd = accept(sockfd, (SA *) &cliaddr, &clilen);
-
-        snprintf(buff, sizeof(buff), "%.24s\r\n", "This Is Mario");
-        write(connfd, buff, strlen(buff));
+        if ( (childpid = fork()) == 0) {	/* child process */
+            close(sockfd);	    /* close listening socket */
+            str_echo(connfd);	/* process the request */
+            exit(0);
+        }
         close(connfd);
+    }
+}
+
+void str_echo(int sockfd) {
+    ssize_t n;
+    char buf[MAXLINE];
+
+    again:
+        while ((n = read(sockfd, buf, MAXLINE)) > 0) {
+            write(sockfd, buf, n);
+        }
+
+    if (n < 0 && errno == EINTR)
+        goto again;
+    else if (n < 0){
+        printf("str_echo: read error");
+        exit(0);
     }
 }
